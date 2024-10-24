b72418bfa7789959cca93504524723f61e2bb56f
Mohit Kumar Garg <162031741+DevMohitgarg@users.noreply.github.com>
2024-10-24 11:00:44 +0530

    Time: 179 ms (5.17%) | Memory: 21.8 MB (89.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int lastNonZeroIndex = 0;
+        int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                nums[lastNonZeroIndex] = nums[i];
-                lastNonZeroIndex++;
+        for (int i = 0; i < nums.size(); i++) {
+            if(nums[i]==0)
+            {
+                count++;
+                nums.erase(nums.begin()+i);
+                i--;
             }
         }
-
-        for (int i = lastNonZeroIndex; i < n; i++) {
-            nums[i] = 0;
-        }
+         for(int i=0;i<count;i++)
+         {
+            nums.push_back(0);
+         }
+         for(auto it:nums)
+         {
+            cout<<it<<" ";
+         }
     }
 };
