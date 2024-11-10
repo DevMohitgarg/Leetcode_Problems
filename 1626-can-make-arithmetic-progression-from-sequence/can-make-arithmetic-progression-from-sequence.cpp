17414175b787edc8e51f9545ac7bb86bcd36765d
Mohit Kumar Garg <162031741+DevMohitgarg@users.noreply.github.com>
2024-11-10 22:38:39 +0530

    Time: 1 ms (15.59%) | Memory: 11.7 MB (89.14%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        if (arr.size() < 2) {
+            return true;
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int commonDifference = arr[1] - arr[0];
+
+        for (int i = 2; i < arr.size(); ++i) {
+            if (arr[i] - arr[i - 1] != commonDifference) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
